a650d0b0cbec24420aecd588923e898c3a8de5e2
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-06 10:02:31 -0500

    Neopixel traffic light

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Dat Nguyen
+ * Created on: Mar 2026
+ * This program uses the RobotBit's Neopixels to create a traffic light.
 */
 
-basic.showString('Hello, World!')
+// Define colour constants
+const BLACK: NeoPixelColors = NeoPixelColors.Black
+const RED: NeoPixelColors = NeoPixelColors.Red
+const YELLOW: NeoPixelColors = NeoPixelColors.Yellow
+const GREEN: NeoPixelColors = NeoPixelColors.Green
+
+// Initialize display
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Initialize Neopixel strip
+let neopixelStrip: neopixel.Strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, BLACK)
+neopixelStrip.setPixelColor(1, BLACK)
+neopixelStrip.setPixelColor(2, BLACK)
+neopixelStrip.setPixelColor(3, BLACK)
+neopixelStrip.show()
+
+input.onButtonPressed(Button.A, function() {
+    // Turn 3rd Neopixel Green for 1 second
+    neopixelStrip.setPixelColor(2, GREEN)
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(2, BLACK)
+
+    // Turn 2nd Neopixel Yellow for 1 second
+    neopixelStrip.setPixelColor(1, YELLOW)
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(1, BLACK)
+
+    // Turn 1st Neopixel Red for 1 second
+    neopixelStrip.setPixelColor(0, RED)
+    neopixelStrip.show()
+    basic.pause(1000)
+    neopixelStrip.setPixelColor(0, BLACK)
+    neopixelStrip.show()
+})
