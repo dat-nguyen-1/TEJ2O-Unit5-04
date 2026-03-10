5f1443139c09a670af8ef8539e51cfa44660c323
dat-nguyen-1 <dat.nguyen@stu.ocsb.ca>
2026-03-10 09:45:43 -0400

    Refactor comments and fix GREEN color assignment
    Updated comments for clarity and corrected the GREEN color assignment.

--- main.ts
+++ main.ts
@@ -5,19 +5,17 @@
  * This program uses the RobotBit's Neopixels to create a traffic light.
 */
 
-// Define colour constants
+// Assign Neopixel colours as constants.
 const BLACK: NeoPixelColors = NeoPixelColors.Black
 const RED: NeoPixelColors = NeoPixelColors.Red
 const YELLOW: NeoPixelColors = NeoPixelColors.Yellow
-const GREEN: NeoPixelColors = NeoPixelColors.Green
+const GREEN: NeoPixelColors = NeoPixelColors.Gree
 
-
-
-// Initialize display
+// Initialize display.
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-// Initialize Neopixel strip
+// Initialize Neopixel strip.
 const neopixelStrip: neopixel.Strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
 
 neopixelStrip.setPixelColor(0, BLACK)
@@ -26,20 +24,21 @@ neopixelStrip.setPixelColor(2, BLACK)
 neopixelStrip.setPixelColor(3, BLACK)
 neopixelStrip.show()
 
+// Handle input from button A.
 input.onButtonPressed(Button.A, function() {
-    // Turn 3rd Neopixel Green for 1 second
+    // Turn 3rd Neopixel Green for 1 second.
     neopixelStrip.setPixelColor(2, GREEN)
     neopixelStrip.show()
     basic.pause(1000)
     neopixelStrip.setPixelColor(2, BLACK)
 
-    // Turn 2nd Neopixel Yellow for 1 second
+    // Turn 2nd Neopixel Yellow for 1 second.
     neopixelStrip.setPixelColor(1, YELLOW)
     neopixelStrip.show()
     basic.pause(1000)
     neopixelStrip.setPixelColor(1, BLACK)
 
-    // Turn 1st Neopixel Red for 1 second
+    // Turn 1st Neopixel Red for 1 second.
     neopixelStrip.setPixelColor(0, RED)
     neopixelStrip.show()
     basic.pause(1000)
